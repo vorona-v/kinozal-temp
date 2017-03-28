29fa23b5e9b8dfcabb5010e54a2ab39279445fbc
Alecto <fomenkoandrey@inbox.ru>
2017-03-28 13:33:27 +0300

    header

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+(function ($) {  
+
+	/* bootstrap menu hover */
+	$(".dropdown").hover(            
+		function() {
+			$('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+			$(this).toggleClass('open');
+			$('b', this).toggleClass("caret caret-up");                
+		},
+		function() {
+			$('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+			$(this).toggleClass('open');
+			$('b', this).toggleClass("caret caret-up");                
+		});
+
+    
+})(jQuery);                                                                 // Avoid conflicts with other libraries
