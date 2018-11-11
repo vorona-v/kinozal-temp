8be2dc574532da7bcdf63455bd89aa98b0447ddd
Alecto <fomenkoandrey@inbox.ru>
2018-11-11 22:09:03 +0200

    оптимизация кода.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,20 @@
 require('../../gulp/gulp-init.js')();
 
 const comb = require('../../gulp/tasks/comb.js'),
-  scss     = require('../../gulp/tasks/scss.js').scss,
-  mincss   = require('../../gulp/tasks/mincss.js'),
-  uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-  { sync, syncInit } = require('../../gulp/tasks/sync.js');
+    {scss, scssDev, scssMin}     = require('../../gulp/tasks/scss.js'),
+    mincss   = require('../../gulp/tasks/mincss.js'),
+    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+    { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
   syncInit();
-  watch($.path.scss.files, series(scss, mincss));
+  watch($.path.scss.files, series(scssDev));
   watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
   watch($.path.html.files, sync);
 }
 
 task('combScss', comb);
-task('uglifyEs6', series(uglifyes, sync));
+task('uglifyEs6', uglifyes);
 task('sass2minCss', series(scss, mincss));
+task('build', parallel(scss, uglifyes));
 task('watch', watchFiles);
