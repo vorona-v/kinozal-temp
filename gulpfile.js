9316ef37490c68d68f1990892dcf20a1d94ab513
Alecto <fomenkoandrey@inbox.ru>
2018-12-16 22:45:58 +0200

    обновление bootstrap до версии 3.4.0, jQuery до версии 3.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,21 @@
 'use strict';
 
-require('../../gulp/gulp-init.js')();
+require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb.js'),
-        {scss, scssMin, scssDev} = require('../../gulp/tasks/scss.js'),
-        mincss   = require('../../gulp/tasks/mincss.js'),
-        uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-        { sync, syncInit } = require('../../gulp/tasks/sync.js');
+    scss     = require('../../gulp/tasks/scss.js').scss,
+    mincss   = require('../../gulp/tasks/mincss.js'),
+    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+    { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
     syncInit();
-    watch($.path.scss.files, series(scss));
+    watch($.path.scss.files, series(scss, mincss));
     watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
     watch($.path.html.files, sync);
 }
 
 task('combScss', comb);
-task('uglifyEs6', uglifyes);
-task('sass2minCss', scssMin);
-task('devMap', scssDev);
+task('uglifyEs6', series(uglifyes, sync));
+task('sass', series(scss));
 task('watch', watchFiles);
