f6a3224ca529f5cf440593dbf85d04cd85f2ac67
Alecto <fomenkoandrey1978@gmail.com>
2020-06-04 21:02:26 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-'use strict';
+
 
 require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb.js'),
-    scss     = require('../../gulp/tasks/scss.js').scss,
-    mincss   = require('../../gulp/tasks/mincss.js'),
-    uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
-    { sync, syncInit } = require('../../gulp/tasks/sync.js');
+  scss = require('../../gulp/tasks/scss.js').scss,
+  mincss = require('../../gulp/tasks/mincss.js'),
+  uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+  {sync, syncInit} = require('../../gulp/tasks/sync.js');
 
-function watchFiles () {
-    syncInit();
-    watch($.PATH.scss.files, series(scss));
-    watch([$.PATH.js.files, '!' + $.PATH.js.filesMin], series(uglifyes, sync));
-    watch($.PATH.html.files, sync);
+function watchFiles() {
+  syncInit();
+  watch($.PATH.scss.files, series(scss));
+  watch([
+    $.PATH.js.files,
+    `!${$.PATH.js.filesMin}`
+  ], series(uglifyes, sync));
+  watch($.PATH.html.files, sync);
 }
 
-task('combScss', comb);
+task('comb', comb);
 task('uglifyEs6', series(uglifyes, sync));
 task('sass', series(scss));
 task('watch', watchFiles);
